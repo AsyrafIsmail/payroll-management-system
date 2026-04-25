d1e7a72de19cae5a699862b6aacaf56a09d40618
Asyraf Ismail <nikasyraf268@gmail.com>
2026-04-26 00:35:07 +0800

    feat: define payroll calculation service

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,28 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\DepartmentController;
+use App\Http\Controllers\EmployeeController;
+use App\Http\Controllers\PayrollController;
+use App\Http\Controllers\PayslipController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('dashboard');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::middleware(['auth'])->group(function() {
+    Route::view('/dashboard', 'dashboard')->name('dashboard');
+
+    Route::resource('departments', DepartmentController::class);
+    Route::resource('employees', EmployeeController::class);
+
+    Route::get('/payroll/run', [PayrollController::class, 'create'])->name('payroll.create');
+    Route::post('/payroll/run', [PayrollController::class, 'store'])->name('payroll.store');
+    Route::get('/payroll/history', [PayrollController::class, 'index'])->name('payroll.index');
+
+    Route::get('/payslip/{payrollRecord}', [PayslipController::class, 'show'])->name('payslip.show');
+});
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
